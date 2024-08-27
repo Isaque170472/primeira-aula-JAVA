8ee61579874c1bf16b12ad7efbc5100cf0c1c430
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-26 23:14:07 -0300

    first commit

---
+++ src/br/com/fiap/beans/Cliente.java
@@ -0,0 +1,12 @@
+package br.com.fiap.beans;
+
+public class Cliente {
+
+    // visibilidade, tipo de dado e atributo
+    private String nome;
+    private int idade;
+    private double altura;
+
+    // Metodos Setters (entrada) e Getters (Exibir)
+}
+
